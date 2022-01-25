fdccb6b0d5b023705016cb59ec5cdbbc4fda5a89
Mervekurttt <96375907+Mervekurttt@users.noreply.github.com>
2022-01-25 22:32:32 +0300

    Add files via upload

---
+++ mervepythonfinal.py
@@ -0,0 +1,15 @@
+
+import sqlite3
+
+class DB:
+    def _ init_(self):
+        self.bağlan()
+
+        def baglan(self):
+            con_None 
+            try:
+                con=sqlite3 connect ("sayisal.db")
+                print ("bağlandı")
+                except:
+                    print("bir hata gerçekleşti")
+                    return con
